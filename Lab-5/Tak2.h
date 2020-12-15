fd18e1e1bb727851bea1f65b9f147e5f1c717c3c
husnin huse <husen.husni@gmail.com>
2020-12-15 14:27:10 +0100

    commit

---
+++ Lab-5/Tak2.h
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string.h>
+#include"Task 2_lab4.h"
+using namespace std;
+class Shape
+{
+private:
+	Point2D  postion;
+	int colorRGB[4];
+public:
+	Shape(Point2D point, int color_array[4])
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (color_array[i] >= 0 && color_array[i] <= 255)
+				colorRGB[i] = color_array[i];
+		}
+		postion.x = point.x;
+		postion.y = point.y;
+	}
+	virtual void render() = 0;
+	string get_apoint() {
+		return postion.toString();
+	}
+	void get_acolor()
+	{
+		for (int i = 0; i < 4; i++)
+			cout << colorRGB[i];
+		cout << endl;
+	}
+};
+
+
+class Rectangle : public Shape {
+public:
+	float width;
+	float height;
+	Rectangle(Point2D point, float width, float height, int  colorArr[4]) :width(width), height(height), Shape(point, colorArr)
+	{}
+	void render();
+};
+class Triangle : public Shape {
+public:
+	float base;
+	float height;
+	Triangle(Point2D point, float base, float height, int colorArr[4]) : base(base), height(height), Shape(point, colorArr)
+	{}
+	void render();
+
+
+};
+class Circle : public Shape {
+public:
+	float radius;
+	Circle(Point2D point, float radius, int colorArr[4]) :radius(radius), Shape(point, colorArr)
+	{}
+	string point = get_apoint();
+	void render(SDL_renderer* renderer);
+};
+
---
+++ Lab-5/Task2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include "Tak2.h"
+#include"SDL.h"
+#include<vector>
+#include<string>
+using namespace std;
+
+void Circle::render(SDL_renderer *renderer)
+{
+	cout << "Circle" << " position: " << get_apoint() << " Radius: " << radius << endl;
+}
+
+void Rectangle::render()
+{
+	cout << "Rectangle" << " position: " << get_apoint() << " width: " << width << "  height: " << height << endl;
+}
+
+void Triangle::render()
+{
+	cout << "Triangle" << " position: " << get_apoint() << " base: " << base << "  height: " << height << endl;
+}
+
+void testRendering(vector <Shape*> testing)
+{
+	testing[0]->render();
+	testing[1]->render();
+	testing[2]->render();
+
+}
+/*
+int main()
+{
+	Point2D point;
+	point.x = 0;
+	point.y = 1;
+	int color_arr[4] = { 12,12,12,214 };
+	Rectangle first(point, 12.5, 14.7, color_arr);
+	Triangle second(point, 7.5, 12.2, color_arr);
+	Circle thred (point, 6.2, color_arr);
+	vector <Shape*> vector_Shaped;
+	vector_Shaped.push_back(&first);
+	vector_Shaped.push_back(&second);
+	vector_Shaped.push_back(&thred);
+	testRendering(vector_Shaped);
+
+
+
+}*/
